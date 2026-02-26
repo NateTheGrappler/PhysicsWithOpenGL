362463599d26cc11092903e7ec16cc6039312e97
Natan Guzinski <guzinskiNatan@gmail.com>
2026-02-26 13:44:54 -0500

    added in rendering blackholes

--- src/BlackHole2D.cpp
+++ src/BlackHole2D.cpp
@@ -1,4 +1,5 @@
 #include "BlackHole2D.h"
+#include "Scene_Menu.h"
 
 BlackHole2D_Scene::BlackHole2D_Scene(Engine& gameEngine)
 	: Scene(gameEngine)
@@ -15,12 +16,19 @@ void BlackHole2D_Scene::init()
 	glfwSetInputMode(m_engine.getWindow(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
 	//set up user input
-	registerAction(GLFW_KEY_C, "CHANGE_CAMERA");
+	registerAction(GLFW_KEY_C,     "CHANGE_CAMERA");
+	registerAction(GLFW_KEY_ENTER, "CHANGE_SCENE");
 
 	//set up the black hole
-	blackHole2D bh = blackHole2D(8.54 * std::pow(10, 36), glm::vec3(400.0f, 300.0f, 0), glm::vec3(1.0f, 1.0f, 1.0f));
+	blackHole2D bh =
+	{
+		8.54 * std::pow(10, 36),
+		glm::vec3(500.0f, 300.0f, 0),
+		glm::vec3(1.0f, 0.0f, 2.0f),
+		80
+	};
 	m_blackHoles.push_back(bh);
-	std::cout << "Mass: " << bh.mass << "Radius: " << bh.r_s << std::endl;
+	std::cout << "Mass: " << bh.mass << " Radius: " << bh.r_s << " Render Radius: " << bh.getRenderRadius() << std::endl;
 }
 
 void BlackHole2D_Scene::update()
@@ -30,16 +38,17 @@ void BlackHole2D_Scene::update()
 
 void BlackHole2D_Scene::sRender()
 {
-	glDisable(GL_DEPTH_TEST);
 	m_engine.renderer()->clear();
 	m_camera->processInput(m_engine.getWindow(), m_engine.getDeltaTime());
+
+	glDisable(GL_DEPTH_TEST);
 	m_camera->setOrthographic(0.0f, 800.0f, 0.0f, 600.0f, -1.0f, 1.0f);
 	m_engine.renderer()->updateMatrix(m_camera->getProjectionMatrix(), m_camera->getViewMatrix(), m_camera->getPosition());
 	m_engine.renderer()->disableLight();
 
 	for (const blackHole2D& blackHole : m_blackHoles)
 	{
-		m_engine.renderer()->drawCircle(blackHole.position, blackHole.r_s, blackHole.color);
+		m_engine.renderer()->drawCircle(blackHole.position, blackHole.getRenderRadius(), blackHole.color);
 	}
 
 	glEnable(GL_DEPTH_TEST);
@@ -70,9 +79,11 @@ void BlackHole2D_Scene::sUserInput(const Action& action)
 				m_cameraIsStatic = true;
 			}
 		}
+		if (action.name() == "CHANGE_SCENE") { m_engine.changeScene("MENU", std::make_shared<Scene_Menu>(m_engine), false); onEnd(); }
 	}
 }
 
 void BlackHole2D_Scene::onEnd()
 {
+	std::cout << "exited out of scene BLACK_HOLE_2D" << std::endl;
 }
--- src/BlackHole2D.h
+++ src/BlackHole2D.h
@@ -9,15 +9,20 @@ struct blackHole2D
     double mass;
     glm::vec3 position;
     glm::vec3 color;
+    double renderScale;
 
 
     //function to init struct
-    blackHole2D(double mass, glm::vec3 position, glm::vec3 color)
+    blackHole2D(double mass, glm::vec3 position, glm::vec3 color, double pixelRadius)
         :mass(mass), position(position), color(color)
     {
         calculateR_S(299792458, 6.67 * std::pow(10, -11));
+        calculateRenderScale(pixelRadius);
     }
 
+
+    void calculateRenderScale(double pixelRadius) { renderScale = pixelRadius / r_s; }
+    float getRenderRadius() const { return static_cast<float>(r_s * renderScale); }
     void calculateR_S(double speedofLight, double G) { r_s = (2 * G * mass) / (speedofLight * speedofLight); }
 };
 
@@ -34,6 +39,7 @@ private:
     std::vector<blackHole2D> m_blackHoles;
     double m_lightSpeed = 299792458;
     double m_GConstant = 6.67 * std::pow(10, -11);
+    double m_renderScale = 1e-9;   //to scale things down from meters to pixels
 
     bool                     m_cameraIsStatic = true;
     std::unique_ptr<Camera>  m_camera;
